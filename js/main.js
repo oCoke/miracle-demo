2d3ad29d5b384f2682a683cb1d759964e03f10e5
Jun <omycoke@qq.com>
2020-12-23 18:00:51 +0800

    Site updated: 2020-12-23 18:00:50

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(function(){
+    // 将首页 Banner 设为窗口的高度
+    $('#index-banner').css('height', document.body.clientHeight);
+});
+$(function(){
+    $('#page-banner').css('height', document.body.clientHeight/1.8);
+});
